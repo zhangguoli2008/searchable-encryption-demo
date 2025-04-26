03b27624f2e749fd2905f713ce352ecd494242d6
/:Z国力 <285891173@qq.com>
2025-04-27 01:32:40 +1000

    添加benchmark.py 和 visualize.py

---
+++ visualize.py
@@ -0,0 +1,16 @@
+# visualize.py
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.read_csv("benchmark_results.csv")
+# 折线图
+for q in df['query'].unique():
+    sub = df[df['query']==q]
+    plt.plot(sub['d'], sub['avg_latency_ms'], marker='o', label=q)
+plt.xlabel('编辑距离 d')
+plt.ylabel('平均查询延迟 (ms)')
+plt.title('查询延迟对比')
+plt.xticks([0,1,2])
+plt.legend()
+plt.grid(True)
+plt.savefig("latency_plot.png", dpi=300)
